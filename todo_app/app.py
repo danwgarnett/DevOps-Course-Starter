30dfd8813439c885b6e74145089bda681305b19d
Dan Garnett <dan.garnett@mclaren.com>
2020-12-06 15:43:36 +0000

    First exercise, simply listing out the default items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+from todo_app.data.session_items import get_items
 
 from todo_app.flask_config import Config
 
@@ -8,7 +9,16 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    
+    # Get the list of items from the pre-loaded session file
+    todoItems = get_items()
+    
+    # Use a list comprehension to get the titles of all the items
+    todoTitles = [item['title'] for item in todoItems]
+    #todoTitles = ["first item", "second item", "third item"]
+
+    # render the index template, displaying the list items
+    return render_template("index.html", todoItems = todoTitles)
 
 
 if __name__ == '__main__':
